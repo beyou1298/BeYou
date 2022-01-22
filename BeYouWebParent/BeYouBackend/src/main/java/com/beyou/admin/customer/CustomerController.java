46b20729f910441e9befad53276ad32c6297ad0a
beyou1298 <beyoucompany.gp@gmail.com>
2022-01-22 23:02:57 +0530

    customer controller

--- BeYouWebParent/BeYouBackend/src/main/java/com/beyou/admin/customer/CustomerController.java
+++ BeYouWebParent/BeYouBackend/src/main/java/com/beyou/admin/customer/CustomerController.java
@@ -1,6 +1,5 @@
 package com.beyou.admin.customer;
 
-import java.io.IOException;
 import java.util.List;
 
 import com.beyou.admin.paging.PagingAndSortingHelper;
@@ -19,92 +18,85 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class CustomerController {
+	private String defaultRedirectURL = "redirect:/customers/page/1?sortField=firstName&sortDir=asc";
+	
+	@Autowired private CustomerService service;
+	
+	@GetMapping("/customers")
+	public String listFirstPage(Model model) {
+		return defaultRedirectURL;
+	}
 
-    @Autowired
-    private CustomerService service;
+	@GetMapping("/customers/page/{pageNum}")
+	public String listByPage(
+			@PagingAndSortingParam(listName = "listCustomers", moduleURL = "/customers") PagingAndSortingHelper helper,
+			@PathVariable(name = "pageNum") int pageNum) {
 
-    @GetMapping("/customers")
-    public String listFirstPage(Model model){
-        
-        return "redirect:/customers/page/1?sortField=firstName&sortDir=asc";
-    }
+		service.listByPage(pageNum, helper);
+		
+		return "customers/customers";
+	}
+	
+	@GetMapping("/customers/{id}/enabled/{status}")
+	public String updateCustomerEnabledStatus(@PathVariable("id") Integer id,
+			@PathVariable("status") boolean enabled, RedirectAttributes redirectAttributes) {
+		service.updateCustomerEnabledStatus(id, enabled);
+		String status = enabled ? "enabled" : "disabled";
+		String message = "The Customer ID " + id + " has been " + status;
+		redirectAttributes.addFlashAttribute("message", message);
+		
+		return defaultRedirectURL;
+	}	
+	
+	@GetMapping("/customers/detail/{id}")
+	public String viewCustomer(@PathVariable("id") Integer id, Model model, RedirectAttributes ra) {
+		try {
+			Customer customer = service.get(id);
+			model.addAttribute("customer", customer);
+			
+			return "customers/customer_detail_modal";
+		} catch (CustomerNotFoundException ex) {
+			ra.addFlashAttribute("message", ex.getMessage());
+			return defaultRedirectURL;		
+		}
+	}
+	
+	@GetMapping("/customers/edit/{id}")
+	public String editCustomer(@PathVariable("id") Integer id, Model model, RedirectAttributes ra) {
+		try {
+			Customer customer = service.get(id);
+			List<Country> countries = service.listAllCountries();
+			
+			model.addAttribute("listCountries", countries);			
+			model.addAttribute("customer", customer);
+			model.addAttribute("pageTitle", String.format("Edit Customer (ID: %d)", id));
+			
+			return "customers/customer_form";
+			
+		} catch (CustomerNotFoundException ex) {
+			ra.addFlashAttribute("message", ex.getMessage());
+			return defaultRedirectURL;
+		}
+	}
+	
+	@PostMapping("/customers/save")
+	public String saveCustomer(Customer customer, RedirectAttributes ra) {
+		service.save(customer);
+		ra.addFlashAttribute("message", "The customer ID " + customer.getId() + " has been updated successfully.");
+		return defaultRedirectURL;
+	}
 
-    @GetMapping("/customers/page/{pageNum}")
-    public String listByPage( @PagingAndSortingParam(listName = "listCustomers", moduleURL = "/customers") PagingAndSortingHelper helper,
-    @PathVariable(name = "pageNum") int pageNum){
-
-        service.listByPage(pageNum, helper);
-
-        return "customers/customers";
-    }
-
-    @GetMapping("/customers/{id}/enabled/{status}")
-    public String updateCustomerEnabStatus(@PathVariable(name="id") Integer id ,
-    @PathVariable(name="status") boolean enabled , RedirectAttributes redirectAttributes){
-        service.updateCustomerEnabledStatus(id, enabled);
-        String status=enabled ? "enabled" : "disabled";
-        String message = "The Customer ID "+id+" has been "+status;
-        redirectAttributes.addFlashAttribute("message",message);
-
-        return "redirect:/customers";
-    }
-
-    @GetMapping("/customers/detail/{id}")
-    public String viewCustomer(@PathVariable(name = "id")Integer id, Model model, RedirectAttributes ra){
-        try{
-            Customer customer = service.get(id);
-            model.addAttribute("customer", customer);
-
-            return "customers/customer_detail_modal";
-
-        }
-        catch(CustomerNotFoundException ex){
-            ra.addFlashAttribute("message", ex.getMessage());
-            return "redirect:/customers";
-        }
-    }
-
-    @GetMapping("/customers/edit/{id}")
-    public String editCustomer(@PathVariable(name="id") Integer id, Model model,RedirectAttributes ra){
-        try{
-            Customer customer  = service.get(id);
-            List<Country> countries = service.listAllCountries();
-
-            model.addAttribute("listCountries", countries);
-            model.addAttribute("customer", customer);
-            model.addAttribute("pageTitle", String.format("Edit Customer (ID: %d)", id));
-            
-            return "customers/customer_form";
-        }
-        catch(CustomerNotFoundException ex){
-
-            ra.addFlashAttribute("message",ex.getMessage());
-            return "redirect:/customers";
-        }
-    }
-
-    @PostMapping("/customers/save")
-    public String saveCustomer(Customer customer,Model model,
-            RedirectAttributes ra) throws IOException{
-        
-        service.save(customer);
-        ra.addFlashAttribute("message", "The customer ID "+ customer.getId() + " has been updated successfully");
-        return "redirect:/customers";
-    }
-
-    @GetMapping("/customers/delete/{id}")
-    public String deleteCustomer(@PathVariable(name="id") Integer id, Model model,RedirectAttributes ra){
-        try{
-            service.delete(id);
-
-            ra.addFlashAttribute("message",
-            "The customer ID "+id+" has been deleted successfully");
-        }
-        catch(CustomerNotFoundException ex){
-
-            ra.addFlashAttribute("message",ex.getMessage());
-           
-        }
-        return "redirect:/customers";
-    }
+	@GetMapping("/customers/delete/{id}")
+	public String deleteCustomer(@PathVariable Integer id, RedirectAttributes ra) {
+		try {
+			service.delete(id);			
+			ra.addFlashAttribute("message", "The customer ID " + id + " has been deleted successfully.");
+			
+		} catch (CustomerNotFoundException ex) {
+			ra.addFlashAttribute("message", ex.getMessage());
+		}
+		
+		return defaultRedirectURL;
+	}
+	
 }
--- BeYouWebParent/BeYouBackend/src/main/java/com/beyou/admin/customer/CustomerService.java
+++ BeYouWebParent/BeYouBackend/src/main/java/com/beyou/admin/customer/CustomerService.java
@@ -19,74 +19,69 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 public class CustomerService {
+	public static final int CUSTOMERS_PER_PAGE = 10;
+	
+	@Autowired private CustomerRepository customerRepo;
+	@Autowired private CountryRepository countryRepo;	
+	@Autowired private PasswordEncoder passwordEncoder;
+	
+	public void listByPage(int pageNum, PagingAndSortingHelper helper) {
+		helper.listEntities(pageNum, CUSTOMERS_PER_PAGE, customerRepo);
+	}
+	
+	public void updateCustomerEnabledStatus(Integer id, boolean enabled) {
+		customerRepo.updateEnabledStatus(id, enabled);
+	}
+	
+	public Customer get(Integer id) throws CustomerNotFoundException {
+		try {
+			return customerRepo.findById(id).get();
+		} catch (NoSuchElementException ex) {
+			throw new CustomerNotFoundException("Could not find any customers with ID " + id);
+		}
+	}
 
-    public static final int CUSTOMERS_PER_PAGE = 10;
-    
-    @Autowired
-    private CustomerRepository customerRepo;
+	public List<Country> listAllCountries() {
+		return countryRepo.findAllByOrderByNameAsc();
+	}		
+	
+	public boolean isEmailUnique(Integer id, String email) {
+		Customer existCustomer = customerRepo.findByEmail(email);
 
-    @Autowired
-    private CountryRepository countryRepo;
-
-    @Autowired
-    PasswordEncoder passwordEncoder;
-
-    public void listByPage(int pageNum, PagingAndSortingHelper helper){
-        helper.listEntities(pageNum, CUSTOMERS_PER_PAGE, customerRepo);
-    }
-
-    public void updateCustomerEnabledStatus(Integer id, boolean enabled){
-        customerRepo.updateEnabledStatus(id, enabled);
-    }
-
-    public Customer get(Integer id) throws CustomerNotFoundException {
-        try {
-            return customerRepo.findById(id).get();
-        } catch (NoSuchElementException e) {
-            throw new CustomerNotFoundException("Could not find any customer with ID"+ id);        }
-    }
-
-    public List<Country> listAllCountries(){
-        return countryRepo.findAllByOrderByNameAsc();
-    }
-
-    public boolean isEmailUnique(Integer id, String email){
-        Customer existCustomer = customerRepo.findByEmail(email);
-
-        if(existCustomer != null && existCustomer.getId() != id){
-            return false;
-        }
-        
-        return true;
-    }
-
-    public void save(Customer customerInForm){
-
-        Customer customerInDb = customerRepo.findById(customerInForm.getId()).get();
-
-        if(!customerInForm.getPassword().isEmpty()){
-           String encodedPassword = passwordEncoder.encode(customerInForm.getPassword());
-           customerInForm.setPassword(encodedPassword);
-        }
-        else{
-            
-            customerInForm.setPassword(customerInDb.getPassword());
-        }
-        customerInForm.setEnabled(customerInDb.isEnabled());
-        customerInForm.setCreatedTime(customerInDb.getCreatedTime());
-        customerInForm.setVerificationCode(customerInDb.getVerificationCode());
-        customerInForm.setAuthenticationType(customerInDb.getAuthenticationType());
-        customerInForm.setResetPasswordToken(customerInDb.getResetPasswordToken());
-        
-        customerRepo.save(customerInForm);
-    }
-
-    public void delete(Integer id) throws CustomerNotFoundException{
-        Long count = customerRepo.countById(id);
-        if(count == null || count == 0){
-            throw new CustomerNotFoundException("Could not find any customers with ID "+id);
-        }
-
-        customerRepo.deleteById(id);
-    }
+		if (existCustomer != null && existCustomer.getId() != id) {
+			// found another customer having the same email
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public void save(Customer customerInForm) {
+		Customer customerInDB = customerRepo.findById(customerInForm.getId()).get();
+		
+		if (!customerInForm.getPassword().isEmpty()) {
+			String encodedPassword = passwordEncoder.encode(customerInForm.getPassword());
+			customerInForm.setPassword(encodedPassword);			
+		} else {
+			customerInForm.setPassword(customerInDB.getPassword());
+		}		
+		
+		customerInForm.setEnabled(customerInDB.isEnabled());
+		customerInForm.setCreatedTime(customerInDB.getCreatedTime());
+		customerInForm.setVerificationCode(customerInDB.getVerificationCode());
+		customerInForm.setAuthenticationType(customerInDB.getAuthenticationType());
+		customerInForm.setResetPasswordToken(customerInDB.getResetPasswordToken());
+		
+		customerRepo.save(customerInForm);
+	}
+	
+	public void delete(Integer id) throws CustomerNotFoundException {
+		Long count = customerRepo.countById(id);
+		if (count == null || count == 0) {
+			throw new CustomerNotFoundException("Could not find any customers with ID " + id);
+		}
+		
+		customerRepo.deleteById(id);
+	}
+	
 }
